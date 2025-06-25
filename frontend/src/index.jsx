67a05766d0252972ffa5b1862af2d8944f50d4d8
jainnikhil1005 <76232875+jainnikhil1005@users.noreply.github.com>
2025-06-24 21:01:20 -0500

    Update index.jsx

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -3,3 +3,13 @@ import { hydrate, render } from 'react-dom';
 import App from './App';
 import RelayEnvironment from './RelayEnvironment';
 import { RelayEnvironmentProvider } from 'react-relay/hooks';
+
+const rootElement = document.getElementById('root');
+const renderFn = rootElement.hasChildNodes() ? hydrate : render;
+
+renderFn(
+  <RelayEnvironmentProvider environment={RelayEnvironment}>
+    <App />
+  </RelayEnvironmentProvider>,
+  rootElement
+);
