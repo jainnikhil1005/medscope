8f480137348e422c24a4dbb0ee20e7ffbf911799
jainnikhil1005 <76232875+jainnikhil1005@users.noreply.github.com>
2025-06-24 21:00:55 -0500

    Update index.jsx

---
+++ frontend/src/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { hydrate, render } from 'react-dom';
+import App from './App';
+import RelayEnvironment from './RelayEnvironment';
+import { RelayEnvironmentProvider } from 'react-relay/hooks';
