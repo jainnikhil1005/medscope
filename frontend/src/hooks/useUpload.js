420a3af73f820c089da67ced2602b52fe3ec0d35
jainnikhil1005 <76232875+jainnikhil1005@users.noreply.github.com>
2025-06-26 21:47:03 -0500

    Update useUpload.js

--- frontend/src/hooks/useUpload.js
+++ frontend/src/hooks/useUpload.js
@@ -1,3 +1,15 @@
 import { commitMutation } from 'react-relay';
 import graphql from 'babel-plugin-relay/macro';
 import RelayEnvironment from '../RelayEnvironment';
+
+const mutation = graphql`
+  mutation CheckInteractionMutation($p1: Upload!, $p2: Upload!) {
+    checkInteraction(p1: $p1, p2: $p2) {
+      prescription1Id
+      prescription2Id
+      status
+      details
+    }
+  }
+`;
+
