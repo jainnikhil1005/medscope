759fd4709ad94a674ea807498cfd377435f262f2
jainnikhil1005 <76232875+jainnikhil1005@users.noreply.github.com>
2025-06-29 23:24:32 -0500

    Update FileUpload.jsx

--- frontend/src/components/FileUpload.jsx
+++ frontend/src/components/FileUpload.jsx
@@ -5,3 +5,11 @@ export default function FileUpload({ setLoading, setResult }) {
     const [files, setFiles] = useState([]);
     const { commit } = useUpload();
     
+    const onChange = e => setFiles(e.target.files);
+    const onSubmit = () => {
+    setLoading(true);
+    commit(files[0], files[1], res => {
+      setResult(res);
+      setLoading(false);
+    });
+  };
